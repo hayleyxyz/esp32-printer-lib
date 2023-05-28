b4820883d2a12b322ae3a4497bac1ae100b5d702
Hayley Berry <hayley@madokami.com>
2023-05-28 01:59:52 +0100

    Add util.h

---
+++ src/util.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdio>
+#include <cstdint>
+#include <cstddef>
+
+void hex_dump(const uint8_t* data, size_t size)
+{
+    size_t i = 0;
+
+    for (; i < size; ++i)
+    {
+        printf("%02x ", data[i]);
+
+        if ((i + 1) % 16 == 0) {
+            printf("\n");
+        }
+    }
+
+    if (i % 16 != 0) {
+        printf("\n");
+    }
+}
